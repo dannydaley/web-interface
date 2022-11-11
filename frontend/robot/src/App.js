195b6a3932f8fb00acb4b702b265921e9d558d75
Mahi Alavi Khan <mk265905@falmouth.ac.uk>
2022-11-11 20:48:13 +0000

    message

--- frontend/robot/src/App.js
+++ frontend/robot/src/App.js
@@ -1,10 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import Block from './components/block';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Block />
+        <Block />
+        <Block />
+        {/* <block /> */}
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
---
+++ frontend/robot/src/components/block.js
@@ -0,0 +1,13 @@
+import './block.css';
+
+
+export default function block(){
+    
+return(
+    <div className="Block" >
+        <h1>This is the block</h1>
+    </div>
+
+)
+
+}
