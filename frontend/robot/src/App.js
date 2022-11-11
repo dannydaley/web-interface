b505691b9d05a762cdc79935f6d2b19c0bf0e887
Mahi Alavi Khan <mk265905@falmouth.ac.uk>
2022-11-11 21:39:29 +0000

    created login page

--- frontend/robot/src/App.js
+++ frontend/robot/src/App.js
@@ -1,31 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
-import Block from './components/block';
-import Nav from './components/Nav';
+import LoginPage from './components/Login';
+import Navbar from './components/navbar';
+
+
 
 
 function App() {
   return (
     <div className="App">
-      <Nav />
-      <header className="App-header">
-        <Block />
-        <Block />
-        <Block />
-        {/* <block /> */}
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+    <LoginPage />
+    
+    
     </div>
   );
 }
---
+++ frontend/robot/src/components/Login.js
@@ -0,0 +1,10 @@
+import { Container } from "@mui/system";
+
+export default function LoginPage() {
+
+    return(
+        <Container maxWidth="xs">
+            <h1>YOOOOOO</h1>
+        </Container>
+    )
+}
