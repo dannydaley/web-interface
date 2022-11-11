10886e2b0c3942f86512abadaafb725ee7a295f6
dannydaley <dd252935@falmouth.ac.uk>
2022-11-11 21:02:13 +0000

    added material UI to project

--- frontend/robot/src/App.js
+++ frontend/robot/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Block from './components/block';
+import Nav from './components/Nav';
 
 
 function App() {
   return (
     <div className="App">
+      <Nav />
       <header className="App-header">
         <Block />
         <Block />
