71ac33df315894f251447046000b583f90d8c76f
Mahi Alavi Khan <mk265905@falmouth.ac.uk>
2022-11-12 11:39:53 +0000

    added changes

--- frontend/robot/src/App.js
+++ frontend/robot/src/App.js
@@ -2,18 +2,23 @@ import logo from './logo.svg';
 import './App.css';
 import LoginPage from './components/Login';
 import Navbar from './components/navbar';
-
-
+import mainPage from './components/mainPage';
+import * as React from 'react'
 
 
 function App() {
   return (
-    <div className="App" style={{backgroundColor: 'grey', minHeight: '100vh'}}>
+    // <div className="App" style={{backgroundColor: 'grey', minHeight: '100vh', minWidth: '100vw'}}>
 
-    <LoginPage />
-    
-    
+    // {/* <LoginPage /> */}
+    <div>
+      <Navbar />
+      <div>
+      <mainPage />
+      </div>
     </div>
+    
+    // </div>
   );
 }
 
---
+++ frontend/robot/src/components/mainPage.js
@@ -0,0 +1,10 @@
+import * as React from 'react';
+
+export default function mainPage(){
+    return(
+      
+        <h1>helloooooo</h1>
+        
+            
+    )    
+}
